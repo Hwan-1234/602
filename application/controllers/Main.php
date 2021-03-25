6bcdd89302fbd6724c57ac36b645fbf753b8f46d
Hwan-1234 <57241017+Hwan-1234@users.noreply.github.com>
2021-03-25 21:01:17 +0900

    .

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main extends CI_Controller {
-    public function index()
+    public function main()
 	{
 		$this->load->view('welcome_message');
 	}
